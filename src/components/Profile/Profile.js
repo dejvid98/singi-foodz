c1b8f34c919ba6932310ade2700806a256df9782
dejvid98 <davidsucur111@gmail.com>
2019-11-30 16:59:15 +0100

    Updated Profile v2

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,23 +1,27 @@
-import React from "react";
-import Header from "../Home/Header";
-import "./Profile.css";
-import "../Home/Home.css";
+import React from 'react';
+import Header from '../Home/Header';
+import './Profile.css';
+import '../Home/Home.css';
+import Grid from '@material-ui/core/Grid';
 
-import ProfileInfo from "./ProfileInfo";
-import Footer from "../Home/Footer";
+import ProfileInfo from './ProfileInfo';
 
-const profile = props => {
-  return (
-    <div>
-      <Header />
-      <div className="selectBox">
-          <div id="pic"></div>
-          <ProfileInfo value={props.value} handleInput={props.handleInput} />
-          <Footer klas="footer-profile" />
-      </div>
-      
-    </div>
-  );
+const profile = (props) => {
+	return (
+		<div>
+			<Header />
+			<div className="selectBox">
+				<Grid container spacing={1}>
+					<Grid item xs={6}>
+						<div id="pic" />
+					</Grid>
+					<Grid item xs={6}>
+						<ProfileInfo value={props.value} handleInput={props.handleInput} />
+					</Grid>
+				</Grid>
+			</div>
+		</div>
+	);
 };
 
 export default profile;
