f65e5fef8e8dfc3a8d46ea58de8c3f11b9f0e3eb
dejvid98 <davidsucur111@gmail.com>
2019-12-10 14:57:38 +0100

    Cleanup v12.4

--- src/components/Order/BbqOrder.js
+++ src/components/Order/BbqOrder.js
@@ -1,32 +1,31 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../../Context';
-import './Order.css';
-import Header from '../Home/Header';
-import Footer from '../Home/Footer';
-import MenuItem from './MenuItem';
-import Grid from '@material-ui/core/Grid';
-import Fab from '@material-ui/core/Fab';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import MenuItem1 from '../../Assets/BbqMenu/1.jfif';
-import MenuItem2 from '../../Assets/BbqMenu/2.jfif';
-import MenuItem3 from '../../Assets/BbqMenu/3.jfif';
-import MenuItem4 from '../../Assets/BbqMenu/4.jfif';
-import MenuItem5 from '../../Assets/BbqMenu/5.jfif';
-import MenuItem6 from '../../Assets/BbqMenu/6.jfif';
+import React, { useContext } from "react";
+import { AppContext } from "../../Context";
+import "./Order.css";
+import Header from "../Home/Header";
+import Footer from "../Home/Footer";
+import MenuItem from "./MenuItem";
+import Grid from "@material-ui/core/Grid";
+import OrderConfirm from "../micro-components/OrderConfirmModal";
+import MenuItem1 from "../../Assets/BbqMenu/1.jfif";
+import MenuItem2 from "../../Assets/BbqMenu/2.jfif";
+import MenuItem3 from "../../Assets/BbqMenu/3.jfif";
+import MenuItem4 from "../../Assets/BbqMenu/4.jfif";
+import MenuItem5 from "../../Assets/BbqMenu/5.jfif";
+import MenuItem6 from "../../Assets/BbqMenu/6.jfif";
 
 export default function Order(props) {
-	const { reviewsContext } = useContext(AppContext);
-	const { cartContext } = useContext(AppContext);
-	const [ reviews, setReviews ] = reviewsContext;
-	const [ cart, setCart ] = cartContext;
+  const { reviewsContext } = useContext(AppContext);
+  const { cartContext } = useContext(AppContext);
+  const [reviews, setReviews] = reviewsContext;
+  const [cart, setCart] = cartContext;
 
-	const handleCheckout = () => {
-		setReviews([ ...reviews, cart ]);
-		setCart([]);
-	};
+  const handleCheckout = () => {
+    setReviews([...reviews, cart]);
+    setCart([]);
+  };
 
-	const handleCart = (name, price, quantity) => {
-		setCart([
+  const handleCart = (name, price, quantity) => {
+    setCart([
       ...cart,
       {
         restaurant: "Grill 51 Bar",
@@ -37,152 +36,144 @@ export default function Order(props) {
         tajm: new Date().toLocaleTimeString()
       }
     ]);
-	};
+  };
 
-	const ingridients1 = [ 'Cheese', 'Pepperoni', 'Tomato ' ];
-	const ingridients2 = [ 'Mozarella', 'Pepper', 'Flour' ];
+  const ingridients1 = ["Cheese", "Pepperoni", "Tomato "];
+  const ingridients2 = ["Mozarella", "Pepper", "Flour"];
 
-	return (
-		<div>
-			<Header />
-			<div className="selectBox">
-				<h1>Grill 51 Bar</h1>
-				<br />
-				<Grid container spacing={6}>
-					<Grid item xs={4}>
-						<div className="shopping-cart">
-							<h3>Shopping Cart </h3>
-							{cart.length === 0 ? (
-								<p className="cart-info">Your shopping cart is currently empty</p>
-							) : null}
-							{cart.length >= 1 ? (
-								cart.map((item, index) => {
-									return (
-										<div>
-											<hr />
-											<Grid container spacing={1}>
-												<Grid item xs={12}>
-													<h5>{item.name}</h5>
-												</Grid>
-											</Grid>
+  return (
+    <div>
+      <Header />
+      <div className="selectBox">
+        <h1>Grill 51 Bar</h1>
+        <br />
+        <Grid container spacing={6}>
+          <Grid item xs={4}>
+            <div className="shopping-cart">
+              <h3>Shopping Cart </h3>
+              {cart.length === 0 ? (
+                <p className="cart-info">
+                  Your shopping cart is currently empty
+                </p>
+              ) : null}
+              {cart.length >= 1
+                ? cart.map((item, index) => {
+                    return (
+                      <div>
+                        <hr />
+                        <Grid container spacing={1}>
+                          <Grid item xs={12}>
+                            <h5>{item.name}</h5>
+                          </Grid>
+                        </Grid>
 
-											<Grid container spacing={4}>
-												<Grid item xs={6}>
-													Price
-												</Grid>
-												<Grid item xs={6}>
-													Quantity
-												</Grid>
-											</Grid>
+                        <Grid container spacing={4}>
+                          <Grid item xs={6}>
+                            Price
+                          </Grid>
+                          <Grid item xs={6}>
+                            Quantity
+                          </Grid>
+                        </Grid>
 
-											<Grid container spacing={4}>
-												<Grid item xs={6}>
-													{item.price}
-												</Grid>
-												<Grid item xs={6}>
-													{item.quantity}
-												</Grid>
-											</Grid>
-											<hr />
-										</div>
-									);
-								})
-							) : null}
-						</div>
-
-						<div className="checkout-button" onClick={handleCheckout}>
-							<Fab variant="extended" color="secondary" className="checkout-button">
-								<div
-									style={{
-										paddingLeft: '35px',
-										paddingRight: '35px'
-									}}
-								>
-									<ShoppingCartIcon />Checkout
-								</div>
-							</Fab>
-						</div>
-					</Grid>
-					<Grid item xs={8}>
-						<Grid container spacing={2}>
-							<Grid item xs={4}>
-								<MenuItem
-									handleCart={handleCart}
-									title="Pepperoni"
-									price={450}
-									raitingValue={4}
-									image={MenuItem1}
-									ingredients1={ingridients1}
-									ingredients2={ingridients2}
-								/>
-							</Grid>
-							<Grid item xs={4}>
-								<MenuItem
-									handleCart={handleCart}
-									title="Margherita"
-									price={650}
-									raitingValue={5}
-									image={MenuItem2}
-									ingredients1={ingridients1}
-									ingredients2={ingridients2}
-								/>
-							</Grid>
-							<Grid item xs={4}>
-								<MenuItem
-									handleCart={handleCart}
-									title="Cheese"
-									price={250}
-									raitingValue={3}
-									image={MenuItem3}
-									ingredients1={ingridients1}
-									ingredients2={ingridients2}
-								/>
-							</Grid>
-						</Grid>
-						<hr />
-						<Grid container spacing={2}>
-							<Grid item xs={4}>
-								<MenuItem
-									handleCart={handleCart}
-									title="Sausage"
-									price={350}
-									raitingValue={5}
-									image={MenuItem4}
-									ingredients1={ingridients1}
-									ingredients2={ingridients2}
-								/>
-							</Grid>
-							<Grid item xs={4}>
-								<MenuItem
-									handleCart={handleCart}
-									title="Capricciosa"
-									price={750}
-									raitingValue={2}
-									image={MenuItem5}
-									ingredients1={ingridients1}
-									ingredients2={ingridients2}
-								/>
-							</Grid>
-							<Grid item xs={4}>
-								<MenuItem
-									handleCart={handleCart}
-									title="Chicken and Ham"
-									price={400}
-									raitingValue={4}
-									image={MenuItem6}
-									ingredients1={ingridients1}
-									ingredients2={ingridients2}
-								/>
-							</Grid>
-						</Grid>
-					</Grid>
-				</Grid>
-				<Footer klas="footer-order" />
-			</div>
-		</div>
-	);
+                        <Grid container spacing={4}>
+                          <Grid item xs={6}>
+                            {item.price}
+                          </Grid>
+                          <Grid item xs={6}>
+                            {item.quantity}
+                          </Grid>
+                        </Grid>
+                        <hr />
+                      </div>
+                    );
+                  })
+                : null}
+            </div>
+            <div className="checkout-button">
+              <OrderConfirm onClick={handleCheckout}/>
+            </div>
+          </Grid>
+          <Grid item xs={8}>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <MenuItem
+                  handleCart={handleCart}
+                  title="Pepperoni"
+                  price={450}
+                  raitingValue={4}
+                  image={MenuItem1}
+                  ingredients1={ingridients1}
+                  ingredients2={ingridients2}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <MenuItem
+                  handleCart={handleCart}
+                  title="Margherita"
+                  price={650}
+                  raitingValue={5}
+                  image={MenuItem2}
+                  ingredients1={ingridients1}
+                  ingredients2={ingridients2}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <MenuItem
+                  handleCart={handleCart}
+                  title="Cheese"
+                  price={250}
+                  raitingValue={3}
+                  image={MenuItem3}
+                  ingredients1={ingridients1}
+                  ingredients2={ingridients2}
+                />
+              </Grid>
+            </Grid>
+            <hr />
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <MenuItem
+                  handleCart={handleCart}
+                  title="Sausage"
+                  price={350}
+                  raitingValue={5}
+                  image={MenuItem4}
+                  ingredients1={ingridients1}
+                  ingredients2={ingridients2}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <MenuItem
+                  handleCart={handleCart}
+                  title="Capricciosa"
+                  price={750}
+                  raitingValue={2}
+                  image={MenuItem5}
+                  ingredients1={ingridients1}
+                  ingredients2={ingridients2}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <MenuItem
+                  handleCart={handleCart}
+                  title="Chicken and Ham"
+                  price={400}
+                  raitingValue={4}
+                  image={MenuItem6}
+                  ingredients1={ingridients1}
+                  ingredients2={ingridients2}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Footer klas="footer-order" />
+      </div>
+    </div>
+  );
 }
 
 Order.defaultProps = {
-	restaurantName: 'Restaurant Name Props'
+  restaurantName: "Restaurant Name Props"
 };
---
+++ src/components/micro-components/OrderConfirmModal.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Paper from "@material-ui/core/Paper";
+import Fab from "@material-ui/core/Fab";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { Link } from "react-router-dom";
+import ContinueIcon from "../../Assets/Icons/chevron-right-solid.svg";
+
+function PaperComponent(props) {
+  return <Paper {...props} />;
+}
+
+export default function DraggableDialog(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+    props.onClick();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  // <Link to="/reviews" style={{textDecoration : 'none'}}>
+
+  return (
+    <div>
+      <div onClick={handleClickOpen}>
+        <Fab variant="extended" color="secondary" className="checkout-button">
+          <div
+            style={{
+              paddingLeft: "35px",
+              paddingRight: "35px"
+            }}
+          >
+            <ShoppingCartIcon />
+            Checkout
+          </div>
+        </Fab>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          PaperComponent={PaperComponent}
+          style={{ textAlign: "center" }}
+        >
+          <DialogTitle
+            style={{ cursor: "move", textAlign: "center" }}
+            id="draggable-dialog-title"
+          >
+            Your order has been successfully placed!
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText style={{ textAlign: "center" }}>
+              Please head over to reviews page, and let us know much you enjoyed
+              using our service.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/reviews"
+            >
+              <button class="forward">
+                <span>Continue </span>
+              </button>
+            </Link>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </div>
+  );
+}
